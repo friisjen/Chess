2e842ab8af9f4a5ffa8c11c91770ef226c765515
friisjen <friise2001@googlemail.com>
2024-11-25 11:36:10 +0100

    added a panel and painted it with 64 squares

--- src/Board/GameBoard.java
+++ src/Board/GameBoard.java
@@ -9,11 +9,31 @@ import java.awt.event.MouseMotionListener;
 public class GameBoard extends JFrame implements MouseListener, MouseMotionListener {
 
     public GameBoard() {
+        // Init the frame
         JFrame gameWindow = new JFrame("Chess by Friis");
-        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameWindow.setResizable(false);
-        gameWindow.setSize(900, 600);
         gameWindow.setLocationRelativeTo(null);
+
+        // Init the panel to paint the board with the squares
+        JPanel pl = new JPanel(){
+            @Override
+            public void paint(Graphics g) {
+                for(int x = 0; x < 8; x++) {
+                    for(int y = 0; y < 8; y++) {
+                        if((x % 2 == 0 && y % 2 == 0) || (x % 2 != 0 && y % 2 != 0)) {
+                            g.setColor(Color.gray);
+                        } else {
+                            g.setColor(Color.white);
+                        }
+                        g.fillRect(x*64, y*64, 64, 64);
+                    }
+                }
+            }
+        };
+        // Board consists of 64 squares -> 8 * 8
+        pl.setPreferredSize(new Dimension(512, 512));
+        gameWindow.add(pl);
+        gameWindow.pack();
+        gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         gameWindow.setVisible(true);
     }
 
